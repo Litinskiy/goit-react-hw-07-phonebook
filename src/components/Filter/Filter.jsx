03553049f10803a47ee6c6c95547b37d89eefe13
Paul Litinskiy <paul.litinskiy@gmail.com>
2023-01-05 17:24:37 +0100

    React HW No 6 Phonebook/redux/persist completed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -15,7 +15,7 @@ export function Filter() {
           type="text"
           name="filter"
           onChange={e => dispatch(setFilter(e.currentTarget.value))}
-          placeholder="Start enter contact name"
+          placeholder="Enter the contact name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
